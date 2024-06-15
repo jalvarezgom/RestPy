32283f736a0201a2b370f1705b7ed560a045c50c
Jaime AG <ja.alvarezgom@gmail.com>
2024-06-15 13:19:04 +0200

    wip: added new pattern singleton

--- classes/restpy.py
+++ classes/restpy.py
@@ -1,10 +1,8 @@
 from classes.module import RestPyModule
-from utils.singleton_meta import SingletonMeta
+from utils.singleton_meta import SingletonClass
 
 
-class RestPySingleton(RestPyModule, SingletonMeta):
-    ...
+class RestPySingleton(SingletonClass, RestPyModule): ...
 
 
-class RestPy(RestPyModule):
-    ...
+class RestPy(RestPyModule): ...
--- utils/singleton_meta.py
+++ utils/singleton_meta.py
@@ -5,3 +5,12 @@ class SingletonMeta:
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
+
+
+class SingletonClass(SingletonMeta):
+    _is_init = False
+
+    def __init__(self, headers: dict = None, base_url: str = None, base_url_params: list = None, auth_action=None):
+        if not self._is_init:
+            obj = super().__init__(headers, base_url, base_url_params, auth_action)
+            self._is_init = True
